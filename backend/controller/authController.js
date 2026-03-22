d1bb054b369da4d2ea20a979087baae0cc8a53f8
FAT-CAT <ahosanh896@gmail.com>
2026-03-23 01:23:49 +0600

    add auth register functionality and route also email transporter

--- backend/controller/authController.js
+++ backend/controller/authController.js
@@ -1,5 +1,5 @@
 import bcrypt from "bcrypt";
-import jwt from "jsonwebtoken";
+import jwt, { verify } from "jsonwebtoken";
 import userModel from "../models/userModel";
 
 const generateOtp = () => {
@@ -12,4 +12,76 @@ const cookieOptions = {
   sameSite: process.env.Node_ENV === "production" ? "none" : "strict",
 
   maxAge: 7 * 24 * 60 * 60 * 10000,
+};
+
+export const register = async (req, res) => {
+  try {
+    let { name, email, password } = req.body;
+
+    if (!name || !email || !password) {
+      return res.status(400).json({
+        success: false,
+        message: "All fields are required",
+      });
+    }
+
+    email = email.trim().toLowerCase();
+
+    const emailRegex = /^[^\s@]+@[^\s@]+\.[^\s@]+$/;
+
+    if (!emailRegex.test(email)) {
+      return res.status(400).json({
+        success: false,
+        message: "Invalid email format",
+      });
+    }
+
+    const passwordRegex = /^(?=.*[A-Za-z])(?=.*\d)[A-Za-z\d]{8,}$/;
+
+    if (!passwordRegex.test(password)) {
+      return res.status(400).json({
+        success: false,
+        message:
+          "Password must be at least 6 characters and include letters and numbers",
+      });
+    }
+
+    const existingUser = await userModel.findOne({ email });
+
+    if (existingUser) {
+      return res.status(409).json({
+        success: false,
+        message: "User already exists",
+      });
+    }
+
+    const hashedPassword = await bcrypt.hash(password, 10);
+    const otp = generateOtp();
+
+    await userModel.create({
+      name: name.trim(),
+      email,
+      password: hashedPassword,
+      verifyOtp: otp,
+      verifyOtpExpireAt: Date.now + 20 * 60 * 1000,
+    });
+
+    await transporter.sendMail({
+      from: process.env.SENDER_EMAIL,
+      to: email,
+      subject: "Verify your account",
+      text: `Your OTP is ${otp}. It will expire in 5 minutes.`,
+    });
+
+    return res.status(201).json({
+      success: true,
+      message: "OTP sent to email. Please verify your account",
+    });
+  } catch (error) {
+    console.log("REGISTER ERROR:", error);
+    return res.status(500).json({
+      success: false,
+      message: "Internal server error",
+    });
+  }
 };
--- backend/server.js
+++ backend/server.js
@@ -1,21 +1,32 @@
 import express from "express";
 import cors from "cors";
+import cookieParser from "cookie-parser";
 import "dotenv/config";
+
 import connectDB from "./config/mongodb.js";
 import connectCloudinary from "./config/cloudinary.js";
+import authRouter from "./routes/authRoutes.js";
 
 // App config
 const app = express();
 const port = process.env.PORT || 4000;
 
 // middleware
-app.use(cors());
+app.use(
+  cors({
+    origin: "http://localhost:5173", // frontend URL
+    credentials: true,
+  }),
+);
 app.use(express.json());
+app.use(cookieParser());
 
 // routes
 app.get("/", (req, res) => {
   res.send("API Working");
 });
+
+app.use("/api/auth", authRouter);
 
 // start server with DB
 const startServer = async () => {
