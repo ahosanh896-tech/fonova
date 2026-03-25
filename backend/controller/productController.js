8b6f9054eaede446792c4987c0bed113314be4c0
FAT-CAT <ahosanh896@gmail.com>
2026-03-25 14:24:09 +0600

    add getProducts functionality

--- backend/controller/productController.js
+++ backend/controller/productController.js
@@ -87,7 +87,70 @@ export const addProduct = async (req, res) => {
   }
 };
 
-export const getProducts = async (req, res) => {};
+export const getProducts = async (req, res) => {
+  try {
+    const {
+      keyword,
+      category,
+      minPrice,
+      maxPrice,
+      sort,
+      page = 1,
+      limit = 10,
+    } = req.query;
+
+    let query = {};
+
+    if (keyword) {
+      query.$text = { $search: keyword };
+    }
+
+    if (category) {
+      query.category = category;
+    }
+
+    //price filter
+    if (minPrice || maxPrice) {
+      query.price = {};
+      if (minPrice) query.price.$gte = Number(minPrice);
+      if (maxPrice) query.price.$lte = Number(maxPrice);
+    }
+
+    //Only active products
+    query.isActive = true;
+
+    //sorting
+    let sortOption = {};
+    if (sort === "price_asc") sortOption.price = 1;
+    if (sort === "price_desc") sortOption.price = -1;
+    if (sort === "newest") sortOption.createAt = -1;
+
+    //pagination
+    const skip = (Number(page) - 1) * Number(limit);
+
+    const products = await productModel
+      .find(query)
+      .sort(sortOption)
+      .skip(skip)
+      .limit(Number(limit));
+
+    const total = await productModel.countDocuments(query);
+
+    res.json({
+      success: true,
+      total,
+      page: Number(page),
+      pages: Math.ceil(total / limit),
+      products,
+    });
+  } catch (error) {
+    console.error(error);
+    res.status(500).json({
+      success: false,
+      message: error.message,
+    });
+  }
+};
 
 export const getSingleProduct = async (req, res) => {};
 
--- backend/routes/productRoutes.js
+++ backend/routes/productRoutes.js
@@ -1,10 +1,12 @@
 import express from "express";
 import upload from "../middleware/multer.js";
 
-import { addProduct } from "../controller/productController.js";
+import { addProduct, getProducts } from "../controller/productController.js";
 
 const productRouter = express.Router();
 
 productRouter.post("/add", upload.array("images", 4), addProduct);
+
+productRouter.get("/", getProducts);
 
 export default productRouter;
