bae20fadeb8d1aa59c3ed21825c387e3bc7e71fd
FAT-CAT <ahosanh896@gmail.com>
2026-04-30 00:25:46 +0600

    add usePayment custom hook

---
+++ frontend/src/hooks/usePayment.js
@@ -0,0 +1,39 @@
+// hooks/usePayment.js
+import { useState, useCallback } from "react";
+import { api } from "../api";
+import { errorToast } from "../Toast";
+
+export const usePayment = () => {
+  const [loading, setLoading] = useState(false);
+
+  const createStripePayment = useCallback(async ({ items, address }) => {
+    try {
+      setLoading(true);
+
+      const res = await api.post("/stripe/create-session", {
+        items,
+        address,
+      });
+
+      if (res.data.success) {
+        // redirect to Stripe
+        window.location.href = res.data.url;
+      } else {
+        errorToast(res.data.message);
+      }
+
+      return res.data;
+    } catch (error) {
+      const errData = error.response?.data;
+      errorToast(errData?.message || error.message);
+      return errData;
+    } finally {
+      setLoading(false);
+    }
+  }, []);
+
+  return {
+    loading,
+    createStripePayment,
+  };
+};
